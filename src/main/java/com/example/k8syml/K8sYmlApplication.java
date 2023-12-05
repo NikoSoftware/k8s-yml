75256289def14ada319687f2c7bb980477cafc0e
niko <zhongqin.chen@bjbn.net>
2023-12-05 18:52:11 +0800

    k8s 脚本

---
+++ src/main/java/com/example/k8syml/K8sYmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8syml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sYmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sYmlApplication.class, args);
+    }
+
+}
